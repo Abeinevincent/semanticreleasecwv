a0b861c469cf16ad74c6aaeec1a5ab978226b8d6
Abeinevincent <abeinevincent@gmail.com>
2023-04-06 15:05:48 +0300

    fix(index.tsx): updated thing component's placeholder

--- src/index.tsx
+++ src/index.tsx
@@ -11,5 +11,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+  return <div>{children || `testing placeholder text when no children is provided`}</div>;
 };
