05ebcc1eec6a52db5a6448ed332073dcb15d3313
Abeinevincent <abeinevincent@gmail.com>
2023-04-06 13:51:58 +0300

    feat(input): added input in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,28 @@
-export * from "./Input"
+import React, { FC, HTMLAttributes, ReactChild } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
+  children?: ReactChild;
+}
+
+// Please do not use types off of a default export module or else Storybook Docs will suffer.
+// see: https://github.com/storybookjs/storybook/issues/9556
+/**
+ * A custom Thing component. Neat!
+ */
+export const Thing: FC<Props> = ({ children }) => {
+  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+};
+
+export interface InputProps {
+  type: string;
+  width: string;
+}
+
+export const Input = ({ type, width }: InputProps) => {
+  return (
+    <div>
+      <input type={type} style={{ width: width }} />
+    </div>
+  );
+};
